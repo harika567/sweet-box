7f7a5925e6781a9c57614305ea177ae1f5292536
harika <harikaharika1520@gmail.com>
2021-10-07 12:34:53 +0530

    initial commit

---
+++ codemitra1.py
@@ -0,0 +1,9 @@
+num=int(input("eneter the number"))
+for i in range(1,num+1):
+    for i in range(num,0):
+        print("&")
+    for i in range(1,i+1):
+        print("*",end=" ")
+    print("")
+        
+
